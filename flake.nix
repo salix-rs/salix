25d149f7934ffe62a14a293a9a159d8ab6782d61
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-06-13 19:52:54 +0200

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    futils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    futils,
+  } @ inputs: let
+    inherit (nixpkgs) lib;
+    inherit (futils.lib) eachDefaultSystem defaultSystems;
+
+    nixpkgsFor = lib.genAttrs defaultSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [
+          rust-overlay.overlays.default
+        ];
+      });
+  in
+    eachDefaultSystem (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          # (lib.hiPrio rust-bin.nightly.latest.rustfmt)
+          (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+          sccache
+
+          cargo-msrv
+          cargo-release
+          cargo-workspaces
+          git
+          just
+        ];
+
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        RUST_BACKTRACE = 1;
+        RUSTC_WRAPPER = "sccache";
+      };
+    });
+}
