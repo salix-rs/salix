58cf431e530af5b1ea04cea54d634fe0850937ae
Maxime VAILLARD <309nahe@MacBook-Air-de-Maxime.local>
2025-06-19 22:58:05 +0200

    config: add default value for controller address + add env source for conf default location

--- crates/salix-config/src/lib.rs
+++ crates/salix-config/src/lib.rs
@@ -1,11 +1,16 @@
 //! Salix config
 
+use std::os::unix::net::SocketAddr;
 use std::{env::current_dir, path::PathBuf};
 
 use anyhow::Result;
 use serde::Deserialize;
+use serde::Serialize;
+
+//TODO: Add enum + implement in conf structs
 
 #[derive(Deserialize, Clone, Debug)]
+//Controller Config
 pub struct ControllerConfig {
     listen: String,
     cert_path: PathBuf,
@@ -13,17 +18,25 @@ pub struct ControllerConfig {
 }
 
 #[derive(Deserialize, Clone, Debug)]
+//Agent Config
 pub struct AgentConfig {
+    #[serde(default = "default_controller_address")]
     controller_address: String,
     cert_path: PathBuf,
 }
 
 #[derive(Deserialize, Clone, Debug)]
+// General Config
 pub struct Config {
     controller: ControllerConfig,
     agent: AgentConfig,
 }
 
+pub fn default_controller_address() -> String {
+    return "127.0.0.1".to_string();
+}
+
+// Get configuration from sources
 pub fn get_config(config_path: Option<PathBuf>) -> Result<Config> {
     let config_path = if let Some(config_path) = config_path {
         config_path
@@ -36,6 +49,7 @@ pub fn get_config(config_path: Option<PathBuf>) -> Result<Config> {
     // TODO: Add env source
     let settings = config::Config::builder()
         .add_source(config::File::from(config_path))
+        .add_source(config::Environment::with_prefix("SALIX"))
         .build()?;
     Ok(settings.try_deserialize()?)
 }
