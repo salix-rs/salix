87eb5cb41b44d8a63abeae38d203306c696d25f5
309nahe <43817529+309nahe@users.noreply.github.com>
2025-06-24 20:45:00 +0200

    config: improvements (#6)
    Co-authored-by: Maxime VAILLARD <309nahe@MacBook-Air-de-Maxime.local>
    Co-authored-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- crates/salix-config/src/lib.rs
+++ crates/salix-config/src/lib.rs
@@ -1,10 +1,33 @@
 //! Salix config
 
+use log::{self, Level};
 use std::{env::current_dir, net::SocketAddr, path::PathBuf};
 
 use anyhow::Result;
 use serde::Deserialize;
 
+mod default_values;
+mod utils;
+
+fn deserialize_log_level<'de, D>(deserializer: D) -> Result<Level, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let s: String = Deserialize::deserialize(deserializer)?;
+    match s.to_ascii_lowercase().as_str() {
+        "error" => Ok(Level::Error),
+        "warn" => Ok(Level::Warn),
+        "info" => Ok(Level::Info),
+        "debug" => Ok(Level::Debug),
+        "trace" => Ok(Level::Trace),
+        _ => Err(serde::de::Error::custom(format!(
+            "invalid log level: {}",
+            s
+        ))),
+    }
+}
+
+/// Controller Config
 #[derive(Deserialize, Clone, Debug)]
 pub struct ControllerConfig {
     pub listen: SocketAddr,
@@ -12,18 +35,27 @@ pub struct ControllerConfig {
     pub key_path: PathBuf,
 }
 
+/// Agent Config
 #[derive(Deserialize, Clone, Debug)]
 pub struct AgentConfig {
     pub controller_address: String,
+    #[serde(default = "default_values::default_agent_cert_path")]
     pub cert_path: PathBuf,
 }
 
+/// Main Config
 #[derive(Deserialize, Clone, Debug)]
 pub struct Config {
     pub controller: ControllerConfig,
     pub agent: AgentConfig,
+    #[serde(
+        default = "default_values::default_config_log_level",
+        deserialize_with = "deserialize_log_level"
+    )]
+    pub log_level: Level,
 }
 
+/// Get configuration from sources
 pub fn get_config(config_path: Option<PathBuf>) -> Result<Config> {
     let config_path = if let Some(config_path) = config_path {
         config_path
@@ -36,6 +68,7 @@ pub fn get_config(config_path: Option<PathBuf>) -> Result<Config> {
     // TODO: Add env source
     let settings = config::Config::builder()
         .add_source(config::File::from(config_path))
+        .add_source(config::Environment::with_prefix("SALIX"))
         .build()?;
     Ok(settings.try_deserialize()?)
 }
